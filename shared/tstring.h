c91c36675c1c8f41d4396a5c16333b4449e497b0
twostars <i.am.twostars@gmail.com>
2012-09-30 08:20:51 +1000

    Unicode string code is now only implemented in unicode mode (so we keep the singlebyte stuff regardless).

--- shared/tstring.h
+++ shared/tstring.h
@@ -12,6 +12,34 @@ typedef std::wstring tstring;
 typedef std::string tstring;
 #endif
 
+static std::string string_format(const std::string fmt, ...)
+{
+	char buffer[1024];
+	va_list ap;
+
+	va_start(ap, fmt);
+	_vsnprintf_s(buffer, sizeof(buffer), sizeof(buffer), fmt.c_str(), ap);
+	va_end(ap);
+
+	return buffer;
+}
+
+// trim from end
+static inline std::string & rtrim(std::string &s) 
+{
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+static inline void tstrcpy(char *dst, size_t len, std::string & src)
+{
+	memset(dst, 0x00, len);
+	memcpy(dst, src.c_str(), src.length() > len ? len : src.length());
+}
+
+#define _tstrcpy(dst, src) tstrcpy(dst, sizeof(dst), src)
+
+#ifdef UNICODE
 static tstring string_format(const tstring fmt, ...)
 {
 	TCHAR buffer[1024];
@@ -21,10 +49,9 @@ static tstring string_format(const tstring fmt, ...)
 	_vsntprintf_s(buffer, sizeof(buffer), sizeof(buffer), fmt.c_str(), ap);
 	va_end(ap);
 
-	return tstring(buffer);
+	return buffer;
 }
 
-// trim from end
 static inline tstring & rtrim(tstring &s) 
 {
 	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
@@ -36,5 +63,4 @@ static inline void tstrcpy(TCHAR *dst, size_t len, tstring & src)
 	memset(dst, 0x00, len);
 	memcpy(dst, src.c_str(), src.length() > len ? len : src.length());
 }
-
-#define _tstrcpy(dst, src) tstrcpy(dst, sizeof(dst), src)
+#endif
