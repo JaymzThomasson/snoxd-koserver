6622c21fa6092c739397dbaaa8110e081ff8fee6
twostars <i.am.twostars@gmail.com>
2013-02-10 22:05:27 +1100

    Fixed a bug with parsing; the opcode was accidentally included with the packet data.

--- Ebenezer/IOCPSocket2.cpp
+++ Ebenezer/IOCPSocket2.cpp
@@ -515,7 +515,7 @@ void CIOCPSocket2::ReceivedData(int length)
 		// found a packet - it's parse time!
 		Packet pkt(*in_stream, (size_t)m_remaining--);
 		if (m_remaining > 0)
-			pkt.append(in_stream, m_remaining);
+			pkt.append(in_stream + 1, m_remaining);
 		Parsing(pkt);
 		m_remaining = 0;
 	}
