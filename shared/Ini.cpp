3a7e780aa048a81ffdede6bcbe1fcb96dbb9f0ef
twostars <i.am.twostars@gmail.com>
2013-03-13 19:25:24 +1100

    Or, even neater.

--- shared/Ini.cpp
+++ shared/Ini.cpp
@@ -3,72 +3,45 @@
 
 #define INI_BUFFER 512
 
-CIni::CIni()
-{
-}
-
 CIni::CIni(const char *lpFilename)
 {
-	SetPath(lpFilename);
+	m_szFileName = GetProgPath() + lpFilename;
 }
-
 
 int CIni::GetInt(char* lpAppName, char* lpKeyName, int nDefault)
 {
-	if (m_szFileName[0] == 0)
-		return -1;
-
 	char tmp[INI_BUFFER];
-	GetPrivateProfileInt(lpAppName, lpKeyName,nDefault, m_szFileName);
-	if (!GetPrivateProfileString(lpAppName, lpKeyName, "", tmp, INI_BUFFER, m_szFileName))
+	GetPrivateProfileInt(lpAppName, lpKeyName,nDefault, m_szFileName.c_str());
+	if (!GetPrivateProfileString(lpAppName, lpKeyName, "", tmp, INI_BUFFER, m_szFileName.c_str()))
 	{
 		sprintf_s(tmp, INI_BUFFER, "%d", nDefault);
-		WritePrivateProfileString(lpAppName, lpKeyName, tmp, m_szFileName);
+		WritePrivateProfileString(lpAppName, lpKeyName, tmp, m_szFileName.c_str());
 		return nDefault;
 	}
 
-	return GetPrivateProfileInt(lpAppName, lpKeyName, nDefault, m_szFileName);
+	return GetPrivateProfileInt(lpAppName, lpKeyName, nDefault, m_szFileName.c_str());
 }
 
 void CIni::GetString(char* lpAppName, char* lpKeyName, char* lpDefault, char *lpOutString, int nOutLength, bool bAllowEmptyStrings /*= true */)
 {
-	if (m_szFileName[0] == 0)
-	{
-		strcpy_s(lpOutString, nOutLength, ""); // reset it
-		return;
-	}
 	
-	if (!GetPrivateProfileString(lpAppName, lpKeyName, "", lpOutString, nOutLength, m_szFileName)
+	if (!GetPrivateProfileString(lpAppName, lpKeyName, "", lpOutString, nOutLength, m_szFileName.c_str())
 		|| (!bAllowEmptyStrings && lpOutString[0] == 0))
 	{
-		WritePrivateProfileString(lpAppName, lpKeyName, lpDefault, m_szFileName);
+		WritePrivateProfileString(lpAppName, lpKeyName, lpDefault, m_szFileName.c_str());
 		strcpy_s(lpOutString, nOutLength, lpDefault);
 	}
 }
 
-bool CIni::SetPath(const char* lpFilename)
-{
-	char IniPath[_MAX_PATH] = "";
-	sprintf_s(IniPath, sizeof(IniPath), "%s%s", GetProgPath(), lpFilename);
-	strcpy_s(m_szFileName, _MAX_PATH, IniPath);
-	return true;
-}
-
 int CIni::SetInt(char* lpAppName, char* lpKeyName, int nDefault)
 {
-	if (m_szFileName[0] == 0)
-		return -1;
-
 	char tmpDefault[INI_BUFFER];
 	sprintf_s(tmpDefault, INI_BUFFER, "%d", nDefault);
 
-	return WritePrivateProfileString(lpAppName, lpKeyName, tmpDefault, m_szFileName);
+	return WritePrivateProfileString(lpAppName, lpKeyName, tmpDefault, m_szFileName.c_str());
 }
 
 int CIni::SetString(char* lpAppName, char* lpKeyName, char* lpDefault)
 {
-	if (m_szFileName[0] == 0)
-		return -1;
-
-	return WritePrivateProfileString(lpAppName, lpKeyName, lpDefault, m_szFileName);
+	return WritePrivateProfileString(lpAppName, lpKeyName, lpDefault, m_szFileName.c_str());
 }
--- shared/Ini.h
+++ shared/Ini.h
@@ -3,15 +3,13 @@
 class CIni
 {
 private:
-	char m_szFileName[_MAX_PATH];
+	std::string m_szFileName;
 
 public:
-	CIni();
 	CIni(const char *lpFilename);
 
 	int GetInt(char* lpAppName, char* lpKeyName, int nDefault);
 	void GetString(char* lpAppName, char* lpKeyName, char* lpDefault, char *lpOutString, int nOutLength, bool bAllowEmptyStrings = true);
 	int SetInt(char* lpAppName, char* lpKeyName, int nDefault);
 	int SetString(char* lpAppName, char* lpKeyName, char* lpDefault);
-	bool SetPath(const char* lpFilename);
 };
