62adf924048bc8c733c405c41f31398302e91f5c
twostars <i.am.twostars@gmail.com>
2013-03-27 12:35:53 +1100

    Added the bandicoot teeth quest to 16079_Menisia.lua. It's going to be a PITA to do all these quests.

--- Debug/quests/16079_Menisia.lua
+++ Debug/quests/16079_Menisia.lua
@@ -6,48 +6,71 @@
 pUser = nil
 pNpc = nil
 
-local WormSilkSpool			= 110
-local WormSilkSpoolRequest	= 111
-local WormSilkSpoolAccepted	= 112
+local CancelButton				= 0 -- could be any event that doesn't exist
 
-local SilkBundleMenu		= 280	
-local SilkBundleExchange	= 281
+local WormSilkSpool				= 110
+local WormSilkSpoolRequest		= 111
+local WormSilkSpoolAccepted		= 112
+
+local BandicootTeeth			= 200
+local BandicootTeethRequest		= 201
+local BandicootTeethAccepted	= 202
+
+local SilkBundleMenu			= 280	
+local SilkBundleExchange		= 281
 
 function Main(event)
 	-- Map the events to functions
 	local t = {
 		-- Default event for this NPC
-		[190]					= HandleNPCSelection,
-
+		[190]						= HandleNPCSelection,
+		
+		[BandicootTeeth]			= function() pUser:SelectMsg(2, 102, 200, 29, BandicootTeethRequest) end,
+		[BandicootTeethRequest]		= function() pUser:SelectMsg(4, 102, 201, 22, BandicootTeethAccepted, 23, CancelButton) end,
+		[BandicootTeethAccepted]	= function() pUser:SaveEvent(57) end,
+		
 		-- Unknown, possibly not even used.
-		[105]					= Quest105,
-		[120]					= Quest120,
+		[105]						= Quest105,
+		[120]						= Quest120,
+		[205]						= Quest205,
+
+		-- Also unknown, the only one that works (because there's no text references) is 196
+		-- but that shows a map telling you where Menissiah is; the NPC we're talking to. 
+		-- Probably not useful in this case.
+		[195]						= function() pUser:SelectMsg(1, 102, 195, 28, 196) end,
+		[196]						= 
+			function()
+				pUser:ShowMap(5)
+				pUser:SaveEvent(56)
+			end,
+		[197]						= function() pUser:SelectMsg(1, 102, 197, 29, 196) end,
 		
 		-- Worm silk spool quest menu.
-		[WormSilkSpool]			= function() pUser:SelectMsg(2, 100, 150, 29, WormSilkSpoolRequest) end,
+		[WormSilkSpool]				= function() pUser:SelectMsg(2, 100, 150, 29, WormSilkSpoolRequest) end,
 		-- Do you wish to accept this quest? Fancy new dialog. 
-		[WormSilkSpoolRequest]	= function() pUser:SelectMsg(4, 100, 156, 22, WormSilkSpoolAccepted, 23, 193) end, 
+		[WormSilkSpoolRequest]		= function() pUser:SelectMsg(4, 100, 156, 22, WormSilkSpoolAccepted, 23, CancelButton) end, 
 		-- Accepted the worm silk spool quest..
-		[WormSilkSpoolAccepted]	= function() pUser:SaveEvent(7) end,
+		[WormSilkSpoolAccepted]		= function() pUser:SaveEvent(7) end,
 
 		-- Main menu for silk bundle exchanges
-		[SilkBundleMenu]		= function()
+		[SilkBundleMenu]			= function()
 			if (pUser:CheckExistItem(379048000, 5)) then
-				pUser:SelectMsg(2, 100, 158, 4006, SilkBundleExchange, 27, 193)
+				pUser:SelectMsg(2, 100, 158, 4006, SilkBundleExchange, 27, CancelButton)
 			else
-				pUser:SelectMsg(2, 100, 157, 10, 193)
+				pUser:SelectMsg(2, 100, 157, 10, CancelButton)
 			end
 		end,
 		
 		-- The silk bundle exchange itself
-		[SilkBundleExchange]	= function()
+		[SilkBundleExchange]		= function()
 			if (pUser:CheckExchange(1)) then
 				pUser:SaveEvent(8)
 				pUser:RunExchange(1)
 			end
 		end,
 	}
-	
+
+	print("Event: " .. event)
 	local func = t[event]
 	if (func and type(func) == "function") then
 		func()
@@ -60,7 +83,7 @@ function HandleNPCSelection()
 	
 	-- Nothing to do, user's completed everything available for them.
 	if (sQuest == 0) then
-		pUser:SelectMsg(2, -1, 191, 10, 193)
+		pUser:SelectMsg(2, -1, 191, 10, CancelButton)
 	-- Show main quest list
 	elseif (sQuest > 1 and sQuest < 100) then
 		pUser:NpcMsg(193)
@@ -84,8 +107,18 @@ end
 function Quest120()
 	pUser:SaveEvent(9)
 	if (pUser:GetNation() == 1) then
-		pUser:SelectMsg(1, 100, 131, 14, 193)
+		pUser:SelectMsg(1, 100, 131, 14, CancelButton)
 	else
-		pUser:SelectMsg(1, 100, 132, 14, 193)
+		pUser:SelectMsg(1, 100, 132, 14, CancelButton)
+	end
+end
+
+-- Possibly not used
+function Quest205()
+	pUser:SaveEvent(59)
+	if (pUser:GetNation() == 1) then
+		pUser:SelectMsg(1, 102, 206, 32, 189)
+	else
+		pUser:SelectMsg(1, 102, 207, 4080, 189)
 	end
 end
