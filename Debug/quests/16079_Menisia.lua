adba2a6529f924027dfa5150b8d2c2c9bd85dd3a
twostars <i.am.twostars@gmail.com>
2013-03-28 04:37:52 +1100

    Moved 16079_Menisia.lua's event map outside of Main() so it need only be built once.

--- Debug/quests/16079_Menisia.lua
+++ Debug/quests/16079_Menisia.lua
@@ -19,59 +19,59 @@ local BandicootTeethAccepted	= 202
 local SilkBundleMenu			= 280	
 local SilkBundleExchange		= 281
 
+-- Map the events to functions
+local eventMap = {
+	-- Default event for this NPC
+	[190]						= HandleNPCSelection,
+	
+	[BandicootTeeth]			= function() pUser:SelectMsg(2, 102, 200, 29, BandicootTeethRequest) end,
+	[BandicootTeethRequest]		= function() pUser:SelectMsg(4, 102, 201, 22, BandicootTeethAccepted, 23, CancelButton) end,
+	[BandicootTeethAccepted]	= function() pUser:SaveEvent(57) end,
+	
+	-- Unknown, possibly not even used.
+	[105]						= Quest105,
+	[120]						= Quest120,
+	[205]						= Quest205,
+
+	-- Also unknown, the only one that works (because there's no text references) is 196
+	-- but that shows a map telling you where Menissiah is; the NPC we're talking to. 
+	-- Probably not useful in this case.
+	[195]						= function() pUser:SelectMsg(1, 102, 195, 28, 196) end,
+	[196]						= 
+		function()
+			pUser:ShowMap(5)
+			pUser:SaveEvent(56)
+		end,
+	[197]						= function() pUser:SelectMsg(1, 102, 197, 29, 196) end,
+	
+	-- Worm silk spool quest menu.
+	[WormSilkSpool]				= function() pUser:SelectMsg(2, 100, 150, 29, WormSilkSpoolRequest) end,
+	-- Do you wish to accept this quest? Fancy new dialog. 
+	[WormSilkSpoolRequest]		= function() pUser:SelectMsg(4, 100, 156, 22, WormSilkSpoolAccepted, 23, CancelButton) end, 
+	-- Accepted the worm silk spool quest..
+	[WormSilkSpoolAccepted]		= function() pUser:SaveEvent(7) end,
+
+	-- Main menu for silk bundle exchanges
+	[SilkBundleMenu]			= function()
+		if (pUser:CheckExistItem(379048000, 5)) then
+			pUser:SelectMsg(2, 100, 158, 4006, SilkBundleExchange, 27, CancelButton)
+		else
+			pUser:SelectMsg(2, 100, 157, 10, CancelButton)
+		end
+	end,
+	
+	-- The silk bundle exchange itself
+	[SilkBundleExchange]		= function()
+		if (pUser:CheckExchange(1)) then
+			pUser:SaveEvent(8)
+			pUser:RunExchange(1)
+		end
+	end,
+}
+
 function Main(event)
-	-- Map the events to functions
-	local t = {
-		-- Default event for this NPC
-		[190]						= HandleNPCSelection,
-		
-		[BandicootTeeth]			= function() pUser:SelectMsg(2, 102, 200, 29, BandicootTeethRequest) end,
-		[BandicootTeethRequest]		= function() pUser:SelectMsg(4, 102, 201, 22, BandicootTeethAccepted, 23, CancelButton) end,
-		[BandicootTeethAccepted]	= function() pUser:SaveEvent(57) end,
-		
-		-- Unknown, possibly not even used.
-		[105]						= Quest105,
-		[120]						= Quest120,
-		[205]						= Quest205,
-
-		-- Also unknown, the only one that works (because there's no text references) is 196
-		-- but that shows a map telling you where Menissiah is; the NPC we're talking to. 
-		-- Probably not useful in this case.
-		[195]						= function() pUser:SelectMsg(1, 102, 195, 28, 196) end,
-		[196]						= 
-			function()
-				pUser:ShowMap(5)
-				pUser:SaveEvent(56)
-			end,
-		[197]						= function() pUser:SelectMsg(1, 102, 197, 29, 196) end,
-		
-		-- Worm silk spool quest menu.
-		[WormSilkSpool]				= function() pUser:SelectMsg(2, 100, 150, 29, WormSilkSpoolRequest) end,
-		-- Do you wish to accept this quest? Fancy new dialog. 
-		[WormSilkSpoolRequest]		= function() pUser:SelectMsg(4, 100, 156, 22, WormSilkSpoolAccepted, 23, CancelButton) end, 
-		-- Accepted the worm silk spool quest..
-		[WormSilkSpoolAccepted]		= function() pUser:SaveEvent(7) end,
-
-		-- Main menu for silk bundle exchanges
-		[SilkBundleMenu]			= function()
-			if (pUser:CheckExistItem(379048000, 5)) then
-				pUser:SelectMsg(2, 100, 158, 4006, SilkBundleExchange, 27, CancelButton)
-			else
-				pUser:SelectMsg(2, 100, 157, 10, CancelButton)
-			end
-		end,
-		
-		-- The silk bundle exchange itself
-		[SilkBundleExchange]		= function()
-			if (pUser:CheckExchange(1)) then
-				pUser:SaveEvent(8)
-				pUser:RunExchange(1)
-			end
-		end,
-	}
-
 	print("Event: " .. event)
-	local func = t[event]
+	local func = eventMap[event]
 	if (func and type(func) == "function") then
 		func()
 	end
