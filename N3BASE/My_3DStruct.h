be03ed5511227567f3f818ff71592b1fb1c99cd4
twostars <i.am.twostars@gmail.com>
2013-03-29 04:12:56 +1100

    Removed more dependencies to DirectX.

--- N3BASE/My_3DStruct.h
+++ N3BASE/My_3DStruct.h
@@ -50,32 +50,10 @@ public:
 struct __Matrix44 : public D3DXMATRIX // 4x4 Matrix
 {
 public:
+	__Matrix44() {}
 	void Zero();
 	void Identity();
-	const __Vector3 Pos() const;
-	void PosSet(float x, float y, float z);
-	void PosSet(const D3DXVECTOR3& v);
-	void RotationX(float fDelta);
 	void RotationY(float fDelta);
-	void RotationZ(float fDelta);
-	void Rotation(float fX, float fY, float fZ);
-	void Rotation(const D3DXVECTOR3& v);
-	void Scale(float sx, float sy, float sz);
-	void Scale(const D3DXVECTOR3& v);
-
-	void Direction(const D3DXVECTOR3& vDir);
-
-	__Matrix44 operator * (const D3DXMATRIX& mtx);
-	void operator *= (const D3DXMATRIX& mtx);
-	void operator += (const D3DXVECTOR3& v);
-	void operator -= (const D3DXVECTOR3& v);
-
-	void operator = (const D3DXQUATERNION& qt);
-
-	__Matrix44();
-	__Matrix44(const _D3DMATRIX& mtx);
-	__Matrix44(const D3DXMATRIX& mtx);
-	__Matrix44(const D3DXQUATERNION& qt);
 };
 
 inline void	__Vector3::Normalize()
@@ -276,242 +254,16 @@ inline __Vector3 __Vector3::operator / (float fDelta) const
 	return vTmp;
 }
 
-
-inline void __Matrix44::Zero() 
-{
-	memset(this, 0, sizeof(_D3DMATRIX)); 
-}
-
 inline void __Matrix44::Identity()
 {
 	_12 = _13 = _14 = _21 = _23 = _24 = _31 = _32 = _34 = _41 = _42 = _43 = 0;
 	_11 = _22 = _33 = _44 = 1.0f;
 }
 
-inline const __Vector3 __Matrix44::Pos() const 
-{
-	static __Vector3 vTmp;
-
-	vTmp.Set(_41, _42, _43);
-	return vTmp;
-}
-
-inline void __Matrix44::PosSet(float x, float y, float z)
-{
-	_41 = x; _42 = y; _43 = z;
-}
-
-inline void __Matrix44::PosSet(const D3DXVECTOR3& v) 
-{
-	_41 = v.x;
-	_42 = v.y;
-	_43 = v.z;
-}
-
-inline void __Matrix44::RotationX(float fDelta)
-{
-	this->Identity();
-	_22 = cosf(fDelta); _23 = sinf(fDelta); _32 = -_23; _33 = _22;
-}
-
 inline void __Matrix44::RotationY(float fDelta)
 {
 	this->Identity();
 	_11 = cosf(fDelta); _13 = -sinf(fDelta); _31 = -_13; _33 = _11;
-}
-
-inline void __Matrix44::RotationZ(float fDelta)
-{
-	this->Identity();
-	_11 = cosf(fDelta); _12 = sinf(fDelta); _21 = -_12; _22 = _11;
-}
-
-inline void __Matrix44::Rotation(float fX, float fY, float fZ)
-{
-	float SX = sinf(fX), CX = cosf(fX);
-	float SY = sinf(fY), CY = cosf(fY);
-	float SZ = sinf(fZ), CZ = cosf(fZ);
-	_11 = CY * CZ;
-	_12 = CY * SZ;
-	_13 = -SY;
-	_14 = 0;
-	
-	_21 = SX * SY * CZ - CX * SZ;
-	_22 = SX * SY * SZ + CX * CZ;
-	_23 = SX * CY;
-	_24 = 0;
-	
-	_31 = CX * SY * CZ + SX * SZ;
-	_32 = CX * SY * SZ - SX * CZ;
-	_33 = CX * CY;
-	_34 = 0;
-	
-	_41 = _42 = _43 = 0; _44 = 1;
-}
-
-inline void __Matrix44::Rotation(const D3DXVECTOR3& v)
-{
-	float SX = sinf(v.x), CX = cosf(v.x);
-	float SY = sinf(v.y), CY = cosf(v.y);
-	float SZ = sinf(v.z), CZ = cosf(v.z);
-	_11 = CY * CZ;
-	_12 = CY * SZ;
-	_13 = -SY;
-	_14 = 0;
-	
-	_21 = SX * SY * CZ - CX * SZ;
-	_22 = SX * SY * SZ + CX * CZ;
-	_23 = SX * CY;
-	_24 = 0;
-	
-	_31 = CX * SY * CZ + SX * SZ;
-	_32 = CX * SY * SZ - SX * CZ;
-	_33 = CX * CY;
-	_34 = 0;
-	
-	_41 = _42 = _43 = 0; _44 = 1;
-}
-
-inline void __Matrix44::Scale(float sx, float sy, float sz) 
-{
-	this->Identity();
-	_11 = sx; _22 = sy; _33 = sz;
-}
-
-inline void __Matrix44::Scale(const D3DXVECTOR3& v) 
-{
-	this->Identity();
-	_11 = v.x; _22 = v.y; _33 = v.z;
-}
-
-inline __Matrix44::__Matrix44()
-{
-};
-
-inline __Matrix44::__Matrix44(const _D3DMATRIX& mtx)
-{
-	memcpy(this, &mtx, sizeof(_D3DMATRIX));
-}
-
-inline __Matrix44::__Matrix44(const D3DXMATRIX& mtx)
-{
-	memcpy(this, &mtx, sizeof(D3DXMATRIX));
-}
-
-inline __Matrix44::__Matrix44(const D3DXQUATERNION& qt)
-{
-	D3DXMatrixRotationQuaternion(this, &qt);
-}
-
-inline __Matrix44 __Matrix44::operator * (const D3DXMATRIX& mtx)
-{
-	static __Matrix44 mtxTmp;
-
-	mtxTmp._11 = _11 * mtx._11 + _12 * mtx._21 + _13 * mtx._31 + _14 * mtx._41;
-	mtxTmp._12 = _11 * mtx._12 + _12 * mtx._22 + _13 * mtx._32 + _14 * mtx._42;
-	mtxTmp._13 = _11 * mtx._13 + _12 * mtx._23 + _13 * mtx._33 + _14 * mtx._43;
-	mtxTmp._14 = _11 * mtx._14 + _12 * mtx._24 + _13 * mtx._34 + _14 * mtx._44;
-
-	mtxTmp._21 = _21 * mtx._11 + _22 * mtx._21 + _23 * mtx._31 + _24 * mtx._41;
-	mtxTmp._22 = _21 * mtx._12 + _22 * mtx._22 + _23 * mtx._32 + _24 * mtx._42;
-	mtxTmp._23 = _21 * mtx._13 + _22 * mtx._23 + _23 * mtx._33 + _24 * mtx._43;
-	mtxTmp._24 = _21 * mtx._14 + _22 * mtx._24 + _23 * mtx._34 + _24 * mtx._44;
-
-	mtxTmp._31 = _31 * mtx._11 + _32 * mtx._21 + _33 * mtx._31 + _34 * mtx._41;
-	mtxTmp._32 = _31 * mtx._12 + _32 * mtx._22 + _33 * mtx._32 + _34 * mtx._42;
-	mtxTmp._33 = _31 * mtx._13 + _32 * mtx._23 + _33 * mtx._33 + _34 * mtx._43;
-	mtxTmp._34 = _31 * mtx._14 + _32 * mtx._24 + _33 * mtx._34 + _34 * mtx._44;
-
-	mtxTmp._41 = _41 * mtx._11 + _42 * mtx._21 + _43 * mtx._31 + _44 * mtx._41;
-	mtxTmp._42 = _41 * mtx._12 + _42 * mtx._22 + _43 * mtx._32 + _44 * mtx._42;
-	mtxTmp._43 = _41 * mtx._13 + _42 * mtx._23 + _43 * mtx._33 + _44 * mtx._43;
-	mtxTmp._44 = _41 * mtx._14 + _42 * mtx._24 + _43 * mtx._34 + _44 * mtx._44;
-	
-	return mtxTmp;
-}
-
-inline void __Matrix44::operator *= (const D3DXMATRIX& mtx)
-{
-	static __Matrix44 mtxTmp;
-
-	memcpy(&mtxTmp, this, sizeof(__Matrix44));
-
-	_11 = mtxTmp._11 * mtx._11 + mtxTmp._12 * mtx._21 + mtxTmp._13 * mtx._31 + mtxTmp._14 * mtx._41;
-	_12 = mtxTmp._11 * mtx._12 + mtxTmp._12 * mtx._22 + mtxTmp._13 * mtx._32 + mtxTmp._14 * mtx._42;
-	_13 = mtxTmp._11 * mtx._13 + mtxTmp._12 * mtx._23 + mtxTmp._13 * mtx._33 + mtxTmp._14 * mtx._43;
-	_14 = mtxTmp._11 * mtx._14 + mtxTmp._12 * mtx._24 + mtxTmp._13 * mtx._34 + mtxTmp._14 * mtx._44;
-
-	_21 = mtxTmp._21 * mtx._11 + mtxTmp._22 * mtx._21 + mtxTmp._23 * mtx._31 + mtxTmp._24 * mtx._41;
-	_22 = mtxTmp._21 * mtx._12 + mtxTmp._22 * mtx._22 + mtxTmp._23 * mtx._32 + mtxTmp._24 * mtx._42;
-	_23 = mtxTmp._21 * mtx._13 + mtxTmp._22 * mtx._23 + mtxTmp._23 * mtx._33 + mtxTmp._24 * mtx._43;
-	_24 = mtxTmp._21 * mtx._14 + mtxTmp._22 * mtx._24 + mtxTmp._23 * mtx._34 + mtxTmp._24 * mtx._44;
-
-	_31 = mtxTmp._31 * mtx._11 + mtxTmp._32 * mtx._21 + mtxTmp._33 * mtx._31 + mtxTmp._34 * mtx._41;
-	_32 = mtxTmp._31 * mtx._12 + mtxTmp._32 * mtx._22 + mtxTmp._33 * mtx._32 + mtxTmp._34 * mtx._42;
-	_33 = mtxTmp._31 * mtx._13 + mtxTmp._32 * mtx._23 + mtxTmp._33 * mtx._33 + mtxTmp._34 * mtx._43;
-	_34 = mtxTmp._31 * mtx._14 + mtxTmp._32 * mtx._24 + mtxTmp._33 * mtx._34 + mtxTmp._34 * mtx._44;
-
-	_41 = mtxTmp._41 * mtx._11 + mtxTmp._42 * mtx._21 + mtxTmp._43 * mtx._31 + mtxTmp._44 * mtx._41;
-	_42 = mtxTmp._41 * mtx._12 + mtxTmp._42 * mtx._22 + mtxTmp._43 * mtx._32 + mtxTmp._44 * mtx._42;
-	_43 = mtxTmp._41 * mtx._13 + mtxTmp._42 * mtx._23 + mtxTmp._43 * mtx._33 + mtxTmp._44 * mtx._43;
-	_44 = mtxTmp._41 * mtx._14 + mtxTmp._42 * mtx._24 + mtxTmp._43 * mtx._34 + mtxTmp._44 * mtx._44;
-}
-
-inline void __Matrix44::operator += (const D3DXVECTOR3& v)
-{
-	_41 += v.x;
-	_42 += v.y;
-	_43 += v.z;
-}
-
-inline void __Matrix44::operator -= (const D3DXVECTOR3& v)
-{
-	_41 -= v.x;
-	_42 -= v.y;
-	_43 -= v.z;
-}
-
-inline void __Matrix44::operator = (const D3DXQUATERNION& qt)
-{
-	D3DXMatrixRotationQuaternion(this, &qt);
-}
-
-inline void __Matrix44::Direction(const D3DXVECTOR3& vDir)
-{
-	this->Identity();
-
-	static __Vector3 vDir2, vRight, vUp;
-	vUp.Set(0,1,0);
-	vDir2 = vDir;
-	vDir2.Normalize();
-	vRight.Cross(vUp, vDir2); // right = CrossProduct(world_up, view_dir);
-	vUp.Cross(vDir2, vRight); // up = CrossProduct(view_dir, right);
-	vRight.Normalize(); // right = Normalize(right);
-	vUp.Normalize(); // up = Normalize(up);
-
-	_11 = vRight.x; // view(0, 0) = right.x;
-	_21 = vRight.y; // view(1, 0) = right.y;
-	_31 = vRight.z; // view(2, 0) = right.z;
-	_12 = vUp.x; // view(0, 1) = up.x;
-	_22 = vUp.y; // view(1, 1) = up.y;
-	_32 = vUp.z; // view(2, 1) = up.z;
-	_13 = vDir2.x; // view(0, 2) = view_dir.x;
-	_23 = vDir2.y; // view(1, 2) = view_dir.y;
-	_33 = vDir2.z; // view(2, 2) = view_dir.z;
-
-	D3DXMatrixInverse(this, NULL, this);
-	
-//  view(3, 0) = -DotProduct(right, from);
-//  view(3, 1) = -DotProduct(up, from);
-//  view(3, 2) = -DotProduct(view_dir, from);
-
-	// Set roll
-//	if (roll != 0.0f) {
-//		view = MatrixMult(RotateZMatrix(-roll), view);
-//	}
-
-//  return view;
-//} // end ViewMatrix
 }
 
 bool			_IntersectTriangle(const __Vector3& vOrig, const __Vector3& vDir , const __Vector3& v0, const __Vector3& v1, const __Vector3& v2, float& fT, float& fU, float& fV, __Vector3* pVCol = NULL);
@@ -559,12 +311,12 @@ inline bool _IntersectTriangle(const __Vector3& vOrig, const __Vector3& vDir,
     qVec.Cross(tVec, vEdge1);
 
     // Calculate V parameter and test bounds
-    fV = D3DXVec3Dot( &vDir, &qVec );
+    fV = vDir.Dot(qVec);
     if( fV < 0.0f || fU + fV > fDet )
         return FALSE;
 
     // Calculate t, scale parameters, ray intersects triangle
-    fT = D3DXVec3Dot( &vEdge2, &qVec );
+	fT = vEdge2.Dot(qVec);
     float fInvDet = 1.0f / fDet;
     fT *= fInvDet;
     fU *= fInvDet;
@@ -624,12 +376,12 @@ inline bool _IntersectTriangle(const __Vector3& vOrig, const __Vector3& vDir, co
     qVec.Cross(tVec, vEdge1);
 
     // Calculate V parameter and test bounds
-    fV = D3DXVec3Dot( &vDir, &qVec );
+	fV = vDir.Dot(qVec);
     if( fV < 0.0f || fU + fV > fDet )
         return FALSE;
 
     // Calculate t, scale parameters, ray intersects triangle
-    fT = D3DXVec3Dot( &vEdge2, &qVec ) / fDet;
+    fT = vEdge2.Dot(qVec) / fDet;
 
 	// *t < 0 이면 뒤쪽...
 	if ( fT < 0.0f )
