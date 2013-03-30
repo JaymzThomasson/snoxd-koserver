20829f75b1bc8fa61d629385939a0d34f772aa36
twostars <i.am.twostars@gmail.com>
2013-03-31 08:36:17 +1100

    Started porting 18004_Kaisan.lua to our system (doing it in two stages so that you can see exactly how I got from the old code to the new code).

--- Debug/quests/18004_Kaisan.lua
+++ Debug/quests/18004_Kaisan.lua
@@ -1,107 +1,106 @@
 pUser = nil
 pNpc = nil
 
-local UserClass
-local QuestNum
-local Ret = 0
+-- Map the events to functions
+local eventMap = {
+	-- Default event for this NPC
+	[100]						= HandleNPCSelection,
 
-
+	[400]						= Event400,
+	[403]						= Event403,
+	[404]						= Event404,
+	[405]						= Event404,
+	[407]						= Event407,
+	[408]						= Event408,
+	[500]						= Event500,
+	[501]						= Event501,
+	[502]						= Event502,
+	[503]						= Event503,
+}
 
 function Main(event)
+	print("Event: " .. event)
+	local func = eventMap[event]
+	if (func and type(func) == "function") then
+		func()
+	end
+end
 
-if (event == 100) then
-	QuestNum = pUser:SearchQuest()
-		if (QuestNum == 0) then 
-			 pUser:SelectMsg(2, -1, 146, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-			 Ret = 1
-			 elseif (QuestNum > 1) and (QuestNum < 100) then 
-          pUser:NpcMsg(147, 18004)
-      else
-          event = QuestNum
+function HandleNPCSelection()
+	local sQuest = pUser:SearchQuest()
+	if (sQuest == 0) then 
+		pUser:SelectMsg(2, -1, 146, 10, 101)
+	elseif (sQuest > 1) and (sQuest < 100) then 
+		pUser:NpcMsg(147)
+	else
+		Main(sQuest)
+	end
+end
+
+function Event400()
+	local sClass = pUser:GetClass() % 100
+	if (sClass == 1 or sClass == 2 or sClass == 3 or sClass == 4) then
+		pUser:SaveEvent(4061)
+		if (pUser:GetNation() == 1) then
+			pUser:SelectMsg(1, 406, 4062, 4061, 101)
+		else
+			pUser:SelectMsg(1, 406, 4063, 4061, 101)
 		end
 	end
 end
 
-if (event == 101) then
-    Ret = 1
+function Event403()
+	if (pUser:GetLevel() < 10) then
+		pUser:SelectMsg(2, 406, 4068, 10, 101)
+		return
+	end
+	
+	local sClass = pUser:GetClass() % 100
+	if (sClass ~= 1 and sClass ~= 2 and sClass ~= 3 and sClass ~= 4) then
+		pUser:SelectMsg(2, 406, 4068, 10, 101)
+		return
+	end
+
+	if (not pUser:hasCoins(3000)) then
+		pUser:SelectMsg(2, 406, 4065, 10, 101)
+	else
+		pUser:SelectMsg(2, 406, 4064, 4062, 404, 4063, 101)
+	end
 end
 
+function Event404()
+	pUser:SaveEvent(4063)
+	pUser:PromoteUserNovice()
+	pUser:GoldLose(3000)
 
-local savenum = 406
-
-if (event == 400) then
-   Class = pUser:CheckClass()
-   if (Class == 1) or (Class == 2) or (Class == 3) or (Class == 4) then
-      pUser:Saveevent(4061)
-      nation = pUserCheckNation()
-      if (nation == 1) then
-      pUser:SelectMsg(1, savenum, 4062, 4061, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-      else
-      pUser:SelectMsg(1, savenum, 4063, 4061, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-      end
-   else
-   Ret = 1
-   end
+	if (pUser:GetNation() == 1) then
+		pUser:SelectMsg(1, 406, 4066, 4064, 101)
+	else
+		pUser:SelectMsg(1, 406, 4067, 4064, 101)
+	end
 end
 
-if (event == 403) then   
-Level = pUserCheckLevel()
-   if (Level > 9) then 
-   Class = pUser:CheckClass()
-      if (Class == 1) or (Class == 2) or (Class == 3) or (Class == 4) then
-      item_count = pUser:HowmuchItem(900000000)
-        if (item_count < 3000) then 
-         pUser:SelectMsg(2, savenum, 4065, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-         else
-         pUser:SelectMsg(2, savenum, 4064, 4062, 404, 4063, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-         end
-      else
-      pUser:SelectMsg(2, savenum, 4068, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-      end
-   else 
-   pUser:SelectMsg(2, savenum, 4068, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-   end
+function Event407()
+   pUser:SaveEvent(4070)
+   pUser:SelectMsg(2, 407, 4070, 4070, 408, 10, 101)
 end
 
-if (event == 404) then
-   pUser:Saveevent(4063)
-   pUser:PromoteUserNovice()
-   pUser:RobItem(900000000, 3000)
-   nation = pUser:CheckNation()
-   if (nation == 1) then
-   pUser:SelectMsg(1, savenum, 4066, 4064, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-   else
-   pUser:SelectMsg(1, savenum, 4067, 4064, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-   end
+function Event408()
+	pUser:StatPointDistribute()
 end
 
-local savenum = 407
-
-if (event == 407) then
-   pUser:Saveevent(4070)
-   pUser:SelectMsg(2, savenum, 4070, 4070, 408, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
+function Event500()
+	pUser:SelectMsg(3, 453, 4704, 4238, 501, 4239, 502, 4240, 503)
 end
 
-if (event == 408) then
-pUser:StatPointDistribute()
+function Event501()
+	pUser:SelectMsg(9, 453, 4704, 4070, 408, 10, 101)
 end
 
-local savenum = 453
-
-
-if (event == 500) then
-  pUser:SelectMsg(3, savenum, 4704, 4238, 501, 4239, 502, 4240, 503, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
+function Event502()
+	pUser:SelectMsg(9, 453, 4704)
 end
 
-
-if (event == 501) then
-   pUser:SelectMsg(9, savenum, 4704, 4070, 408, 10, 101, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-end
-
-if (event == 502) then
-   pUser:SelectMsg(9, savenum, 4704, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-end
-
-if (event == 503) then
-   pUser:SelectMsg(9, savenum, 4704, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
+function Event503()
+	pUser:SelectMsg(9, 453, 4704)
 end
