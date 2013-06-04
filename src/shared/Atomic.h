18157c86f9d87016f889e44ec4725d2d51d37f41
twostars <i.am.twostars@gmail.com>
2013-06-04 22:48:12 +1000

    Shared: Atomic class should now compile on VS2010. Also hid a signed/unsigned cast warning that's only valid on VS2010 (as the atomic variable is always a long/signed type).

--- src/shared/Atomic.h
+++ src/shared/Atomic.h
@@ -17,6 +17,13 @@ public:
 		template <typename T2> \
 		INLINE friend bool operator op (Atomic<T>& lhs, const T2 rhs) { return lhs.m_atomic op rhs; }
 
+	// Hide signed/unsigned comparison warning.
+	// It's only valid on VS2010, when the type is always long (so, signed).
+	#ifdef WIN32
+	#pragma warning(push)
+	#pragma warning(disable: 4018)
+	#endif
+
 	ATOMIC_COMPARISON(>)
 	ATOMIC_COMPARISON(>=)
 	ATOMIC_COMPARISON(<)
@@ -24,6 +31,9 @@ public:
 	ATOMIC_COMPARISON(==)
 	ATOMIC_COMPARISON(!=)
 
+	#ifdef WIN32
+	#pragma warning(pop)
+	#endif
 	#undef ATOMIC_COMPARISON
 
 #ifdef USE_STD_ATOMIC
@@ -37,8 +47,8 @@ public:
 	INLINE bool compare_exchange(T & expected, T desired) { return m_atomic.compare_exchange_strong(expected, desired); }
 #else
 	template <typename T2> INLINE Atomic<T>& operator=(const T2& rhs) { InterlockedExchange(&m_atomic, rhs); return *this; }
-	template <typename T2> INLINE Atomic<T>& operator+=(const T2 rhs) { InterlockedAdd(&m_atomic, rhs); return *this; }
-	template <typename T2> INLINE Atomic<T>& operator-=(const T2 rhs) { InterlockedAdd(&m_atomic, -rhs); return *this; }
+	template <typename T2> INLINE Atomic<T>& operator+=(const T2 rhs) { InterlockedExchangeAdd(&m_atomic, rhs); return *this; }
+	template <typename T2> INLINE Atomic<T>& operator-=(const T2 rhs) { long val = rhs; InterlockedExchangeAdd(&m_atomic, -val); return *this; }
 
 	INLINE T increment() { return (T) InterlockedIncrement(&m_atomic); }
 	INLINE T decrement() { return (T) InterlockedDecrement(&m_atomic); }
