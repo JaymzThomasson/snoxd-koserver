bc9911e598f6065d9282e451655010f94c31579b
twostars <i.am.twostars@gmail.com>
2013-06-06 22:47:13 +1000

    Shared/config: removed the Windows INI code (no reason to ever have to use it).

--- src/shared/Ini.h
+++ src/shared/Ini.h
@@ -1,12 +1,15 @@
 #pragma once
 
-#define USE_CUSTOM_INI_PARSER
+
+#define	INI_SECTION_START	'['
+#define	INI_SECTION_END		']'
+#define	INI_KEY_SEPARATOR	'='
+#define	INI_NEWLINE			"\r\n"
 
 class CIni
 {
 private:
 	std::string m_szFileName;
-#ifdef USE_CUSTOM_INI_PARSER
 	// Defines key/value pairs within sections
 	typedef std::map<std::string, std::string> ConfigEntryMap;
 
@@ -14,22 +17,12 @@ private:
 	typedef std::map<std::string, ConfigEntryMap> ConfigMap;
 
 	ConfigMap m_configMap;
-#endif
 
 public:
 	CIni(const char *lpFilename);
 
-#ifdef USE_CUSTOM_INI_PARSER
-
-#	define	INI_SECTION_START	'['
-#	define	INI_SECTION_END		']'
-#	define	INI_KEY_SEPARATOR	'='
-#	define	INI_NEWLINE			"\r\n"
-
 	bool Load(const char * lpFileName = nullptr);
 	void Save(const char * lpFileName = nullptr);
-
-#endif
 
 	int GetInt(char* lpAppName, char* lpKeyName, int nDefault);
 	bool GetBool(char* lpAppName, char* lpKeyName, bool bDefault);
