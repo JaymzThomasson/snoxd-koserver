5674c4f7fd3b6e5aac3cad13fa4c45c14cdc5bb8
twostars <i.am.twostars@gmail.com>
2013-06-12 00:13:02 +1000

    AI server: removed an unnecessary case using MSSQL-specific name specifiers.

--- src/shared/database/MakeItemGroupSet.h
+++ src/shared/database/MakeItemGroupSet.h
@@ -7,7 +7,7 @@ public:
 		: OdbcRecordset(dbConnection), m_pMap(pMap) {}
 
 	virtual tstring GetTableName() { return _T("MAKE_ITEM_GROUP"); }
-	virtual tstring GetColumns() { return _T("[iItemGroupNum],[iItem_1],[iItem_2],[iItem_3],[iItem_4],[iItem_5],[iItem_6],[iItem_7],[iItem_8],[iItem_9],[iItem_10],[iItem_11],[iItem_12],[iItem_13],[iItem_14],[iItem_15],[iItem_16],[iItem_17],[iItem_18],[iItem_19],[iItem_20],[iItem_21],[iItem_22],[iItem_23],[iItem_24],[iItem_25],[iItem_26],[iItem_27],[iItem_28],[iItem_29],[iItem_30]"); }
+	virtual tstring GetColumns() { return _T("iItemGroupNum, iItem_1, iItem_2, iItem_3, iItem_4, iItem_5, iItem_6, iItem_7, iItem_8, iItem_9, iItem_10, iItem_11, iItem_12, iItem_13, iItem_14, iItem_15, iItem_16, iItem_17, iItem_18, iItem_19, iItem_20, iItem_21, iItem_22, iItem_23, iItem_24, iItem_25, iItem_26, iItem_27, iItem_28, iItem_29, iItem_30"); }
 
 	virtual bool Fetch()
 	{
