2fc9ca540e9786d9d9d56f6406dd15e1d899c73c
twostars <i.am.twostars@gmail.com>
2013-06-15 08:02:55 +1000

    AI server: monsters cast skills now after the cast time expires (really badly, temporarily..). Previously this was up to the client, but we block that (players controlling skill hits is arguably even more dumb than blocking NPC threads).

--- src/AIServer/NpcMagicProcess.cpp
+++ src/AIServer/NpcMagicProcess.cpp
@@ -7,4 +7,19 @@ void CNpcMagicProcess::MagicPacket(uint8 opcode, uint32 nSkillID, int16 sCasterI
 	Packet result(AG_MAGIC_ATTACK_REQ, opcode);
 	result << nSkillID << sCasterID << sTargetID << sData1 << sData2 << sData3;
 	g_pMain->Send(&result);
+
+	// NOTE: Client controls skill hits.
+	// Since we block these, we need to handle this ourselves.
+	// For now, we'll just block the NPC's thread... 
+	// this WILL need to be rewritten once spawns are though, as it is HORRIBLE.
+	if (opcode != MAGIC_EFFECTING)
+	{
+		_MAGIC_TABLE * pSkill = g_pMain->m_MagictableArray.GetData(nSkillID);
+		if (pSkill == nullptr)
+			return;
+
+		sleep(pSkill->bCastTime * SECOND);
+		result.put(0, uint8(MAGIC_EFFECTING));
+		g_pMain->Send(&result);
+	}
 }
