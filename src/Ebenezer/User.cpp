8be5792781f20bc02bfcd9f84840c8a9dc4ddb0b
twostars <i.am.twostars@gmail.com>
2013-06-29 23:43:40 +1000

    Game server: AP calculation should now correctly apply the AP bonuses (not sure what happened to the parantheses there).

--- src/Ebenezer/User.cpp
+++ src/Ebenezer/User.cpp
@@ -1508,9 +1508,9 @@ void CUser::SetUserAbility(bool bSendPacket /*= true*/)
 
 	m_sMaxWeight = (((GetStatWithItemBonus(STAT_STR) + GetLevel()) * 50) + m_sMaxWeightBonus)  * (m_bMaxWeightAmount / 100);
 	if (isRogue()) 
-		m_sTotalHit = (short)((((0.005f * sItemDamage * (temp_dex + 40)) + ( hitcoefficient * sItemDamage * GetLevel() * temp_dex )) + 3) * (m_bAttackAmount / 100)) * ((100 + m_byAPBonusAmount) / 100);
+		m_sTotalHit = (short)(((((0.005f * sItemDamage * (temp_dex + 40)) + ( hitcoefficient * sItemDamage * GetLevel() * temp_dex )) + 3) * m_bAttackAmount / 100) * (100 + m_byAPBonusAmount) / 100);
 	else
-		m_sTotalHit = (short)(((((0.005f * sItemDamage * (temp_str + 40)) + ( hitcoefficient * sItemDamage * GetLevel() * temp_str )) + 3) * (m_bAttackAmount / 100)) + baseAP) * ((100 + m_byAPBonusAmount) / 100);
+		m_sTotalHit = (short)((((((0.005f * sItemDamage * (temp_str + 40)) + ( hitcoefficient * sItemDamage * GetLevel() * temp_str )) + 3) * m_bAttackAmount / 100) + baseAP) * (100 + m_byAPBonusAmount) / 100);
 
 	m_sTotalAc = (short)(p_TableCoefficient->AC * (GetLevel() + m_sItemAc));
 	m_fTotalHitrate = ((1 + p_TableCoefficient->Hitrate * GetLevel() *  temp_dex ) * m_sItemHitrate/100 ) * (m_bHitRateAmount/100);
