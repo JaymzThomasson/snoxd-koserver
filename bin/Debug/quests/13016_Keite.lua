6e100560f03e0bae30c74d0f10490b3336d9b909
Annunaki <meatbeef@hotmail.co.uk>
2013-07-21 18:47:54 +0100

    Added Magic Bag exchange.
    Added Check for Pet name change certificate.

--- bin/Debug/quests/13016_Keite.lua
+++ bin/Debug/quests/13016_Keite.lua
@@ -37,20 +37,28 @@ function Decline()
 						return
 end
 
-function FamiliarHatchTrans() -- Some sort of "anvil type" U.I should open where you can either hatch or transform an EGG!
+function FamiliarHatchTrans() -- Some sort of "anvil type" U.I should open where you can either hatch or transform an EGG into a PET!
 						return
 end
 
-function FamiliarNameChange() -- Name Change U.I should open where you can change the name of your PET!
+function FamiliarNameChange()
+	if (pUser:CheckExistItem(800090000)) then
+						return  -- Name Change U.I should open where you can change the name of your PET!
+	else
+						pUser:SelectMsg(2,-1,824,10,3001)
+	end
+end
+
+function FamiliarRandom() -- Opens up a Shop for your little fluffy one where you can buy milk, bread and sort!
 						return
 end
 
-function FamiliarRandom() -- Have no idea YET!
-						return
-end
-
-function ExchangeVoucherMagicBag() -- Erm is this the Character Magic bag or do Pets have a bag??
-						return
+function ExchangeVoucherMagicBag()
+	if (pUser:RobItem(800440000)) then
+						pUser:GiveItem(700011001)
+	else
+						pUser:SelectMsg(2,-1,824,10,3001)
+	end
 end
 
 function ExchangeVoucherAutoLoot()
